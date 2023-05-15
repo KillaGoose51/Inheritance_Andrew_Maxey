2303d5f1f2b7ad9eaabc131a0eb15c0cc00f3010
KillaGoose51 <andrew.maxey825@gmail.com>
2023-05-15 15:15:30 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Inheritance_Andrew_Maxey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FPS myFPS = new FPS("E", "ZombCube");
+
+            Console.WriteLine(myFPS.Describe());
+            myFPS.PlayGame();
+
+            FPS csGo = new FPS("M", "CS:GO");
+            Console.WriteLine(csGo.Describe());
+            myFPS.PlayGame();
+        }
+    }
+}
